6f4c6c366f19266d53c8df532b961e97d060620e
RanKa110 <powerun44@gmail.com>
2025-05-09 12:55:23 +0900

    UI제작
    메인화면

---
+++ Assets/Script/UITest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour
+{
+    [SerializeField] private GameObject OptionWindow = null;
+
+    void Start()
+    {
+        OptionWindow.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActiveOptionWindow()
+    {
+        if (!OptionWindow.active)
+        {
+            OptionWindow.SetActive (true);
+        }
+        else
+        {
+            OptionWindow.SetActive (false);
+        }
+    }
+}
