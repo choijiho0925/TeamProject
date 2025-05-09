fc4fc67d1e38cb320ff7c71f0914b6760eadf2ec
l0mini <l0mini@naver.com>
2025-05-09 16:42:34 +0900

    feat : 문 애니메이션 및 프리펩생성

--- Assets/GameScene/Scripts/Door.cs
+++ Assets/GameScene/Scripts/Door.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public float requiredStayTime = 2f; //클리어까지 문앞에 대기해야 하는시간, 애니메이션을 넣는다면 
+    public float requiredStayTime = 3f; //클리어까지 문앞에 대기해야 하는시간, 애니메이션을 넣는다면 
     private float stayTimer = 0f; //문앞에 대기한 시간
     public LayerMask firePlayerLayer; //인스펙터에 퉁사후르 레이어 설정표시
+    public Animator doorAnimation;
+
+    private void Awake()
+    {
+        doorAnimation = GetComponentInChildren<Animator>();
+    }
     private void OnTriggerStay2D(Collider2D collision)// 리지드바디 naverStop안키면 작동 안함
     {
             if ((firePlayerLayer.value & (1 << collision.gameObject.layer)) != 0)
         {
                 stayTimer += Time.deltaTime; // 대기시간 더해줌
-                Debug.Log(stayTimer);
-                if (stayTimer > requiredStayTime)
-                {
-                    Debug.Log("작동 조건 충족");
+                doorAnimation.SetBool("IsClear", true); //문열리는 애니메이션
+            if (stayTimer > requiredStayTime)
+            {     
                     //GameManager.Instance.Clear(); 클리어되는 함수
             }
         }
@@ -26,6 +31,7 @@ public class Door : MonoBehaviour
         if ((firePlayerLayer.value & (1 << collision.gameObject.layer)) != 0)
         {
             stayTimer = 0f;
+            doorAnimation.SetBool("IsClear", false);
         }
     }
 
@@ -40,7 +46,7 @@ public class Door : MonoBehaviour
     //{
     //    //플레이어 사망 애니메이션 출력
     //    Destroy(gameObject);
-    //    isPlayingGame = false;
+    //    Gamemanager.Instance.isPlayingGame = false;
     //    실패 UI SetActive = true
     //}
 }
