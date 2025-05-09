668931586a91288e4194176dc34445256a07c45d
l0mini <l0mini@naver.com>
2025-05-09 13:06:07 +0900

    refactor : 문 스크립트 개선
    퉁사우르의 리지드바디 설정을 neverSleep으로 해주어야한다

--- Assets/GameScene/Scripts/Door.cs
+++ Assets/GameScene/Scripts/Door.cs
@@ -9,18 +9,15 @@ public class Door : MonoBehaviour
     public LayerMask firePlayerLayer;
     private void OnTriggerStay2D(Collider2D collision)
     {
-
-        if ((firePlayerLayer.value & (1 << collision.gameObject.layer)) != 0)
+            if ((firePlayerLayer.value & (1 << collision.gameObject.layer)) != 0)
         {
-            Debug.Log("퉁사후르 감지됨");
-            stayTimer += Time.deltaTime;
-
-            if (stayTimer > requiredStayTime)
-            {
-                Debug.Log("작동 조건 충족");
-                //문에 도달하여 클리어되는 함수();
-                this.enabled = false;
-            }
+                stayTimer += Time.deltaTime;
+                Debug.Log(stayTimer);
+                if (stayTimer > requiredStayTime)
+                {
+                    Debug.Log("작동 조건 충족");
+                    //문에 도달하여 클리어되는 함수();
+                }
         }
     }
 
