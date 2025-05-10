6bc30747432e733c1a8499db56a206905dd35bfb
choijiho0925 <jiho.0925.choi@gmail.com>
2025-05-10 22:49:39 +0900

    fix : 씬이 바뀔때 오브젝트 파괴, UI 출력 버그 수정

--- Assets/GameScene/Scripts/UI/GameUIController.cs
+++ Assets/GameScene/Scripts/UI/GameUIController.cs
@@ -12,8 +12,8 @@ public class GameUIController : MonoBehaviour
     [SerializeField] private GameObject gameUI; // 게임 UI
     [SerializeField] private GameObject settingtUI; // 셋팅 UI
     [SerializeField] private GameObject resultUI; // 스테이지 선택 UI
-    [SerializeField] private GameObject restartButton; // 재시작 버튼
-    [SerializeField] private GameObject timeUI; // 시간 UI
+    [SerializeField] public GameObject restartButton; // 재시작 버튼
+    [SerializeField] public GameObject timeUI; // 시간 UI
 
     TimeUIHandler timeUIHandler;
 
@@ -34,12 +34,12 @@ public class GameUIController : MonoBehaviour
 
     private void Start()
     {
-        timeUIHandler = gameObject.GetComponentInChildren<TimeUIHandler>(); // TimeUIHandler 컴포넌트 가져오기       
+        timeUIHandler = timeUI.GetComponentInChildren<TimeUIHandler>(); // TimeUIHandler 컴포넌트 가져오기       
     }
 
     private void Update()
     {
-        if (GameManager.Instance.isPlayingGame == false && GameManager.Instance.isSuccess == true)
+        if (GameManager.Instance.isResult == true)
         {
             Result();
         }
@@ -59,12 +59,19 @@ public class GameUIController : MonoBehaviour
 
     public void StageSelected()
     {
-        GameManager.Instance.isPlayingGame = false; // 게임 진행 중지
         SceneManager.LoadScene("StageScene"); // 스테이지 씬으로 이동
+        timeUIHandler.playTime = 0f; // 플레이 시간 초기화
         settingtUI.SetActive(false); // 셋팅 UI 비활성화
         resultUI.SetActive(false); // 결과 UI 비활성화
         gameUI.SetActive(true); // 게임 UI 활성화
+        restartButton.SetActive(false); // 재시작 버튼 비활성화
+        timeUI.SetActive(false); // 시간 UI 비활성화
+        GameManager.Instance.opption1 = false; // 하트1 초기화
+        GameManager.Instance.opption2 = false; // 하트2 초기화
+        GameManager.Instance.opption3 = false; // 하트3 초기화
+        GameManager.Instance.isPlayingGame = false; // 게임 진행 중지
         GameManager.Instance.isSuccess = false; // 게임 성공 상태 초기화
+        GameManager.Instance.isResult = false; // 결과창 비활성화
     }
 
     public void Restart()
@@ -73,8 +80,12 @@ public class GameUIController : MonoBehaviour
         timeUIHandler.playTime = 0f; // 플레이 시간 초기화
         settingtUI.SetActive(false); // 셋팅 UI 비활성화
         resultUI.SetActive(false); // 결과 UI 비활성화
+        GameManager.Instance.opption1 = false; // 하트1 초기화
+        GameManager.Instance.opption2 = false; // 하트2 초기화
+        GameManager.Instance.opption3 = false; // 하트3 초기화
         GameManager.Instance.isSuccess = false; // 게임 성공 상태 초기화
         GameManager.Instance.isPlayingGame = true; // 게임 진행
+        GameManager.Instance.isResult = false; // 결과창 비활성화
     }
 
     public void NextStage()
@@ -82,11 +93,14 @@ public class GameUIController : MonoBehaviour
         GameManager.Instance.isPlayingGame = true; // 게임 진행 시작
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // 현재 씬 인덱스
         SceneManager.LoadScene(currentSceneIndex + 1); // 다음 씬으로 이동
+        timeUIHandler.playTime = 0f; // 플레이 시간 초기화
         settingtUI.SetActive(false); // 셋팅 UI 비활성화
         resultUI.SetActive(false); // 결과 UI 비활성화
         GameManager.Instance.opption1 = false; // 하트1 초기화
         GameManager.Instance.opption2 = false; // 하트2 초기화
         GameManager.Instance.opption3 = false; // 하트3 초기화
+        GameManager.Instance.isSuccess = false; // 게임 성공 상태 초기화
+        GameManager.Instance.isResult = false; // 결과창 비활성화
     }
 
     public void Result()
